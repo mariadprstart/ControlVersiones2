680d53de9a8cc401015511b5e91942ec27ec8ccb
mariadprstart <mariadprstart@hotmail.com>
2025-03-20 13:09:54 +0100

    Primera confirmacion

---
+++ src/controlversiones2/ControlVersiones2.java
@@ -0,0 +1,17 @@
+/**/
+package controlversiones2;
+
+/**
+ *
+ * @author Maria8
+ */
+public class ControlVersiones2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primera línea");
+    }
+    
+}
