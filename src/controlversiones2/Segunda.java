b30ddb79c7351fab92469d710aea19385ca9ba2f
mariadprstart <mariadprstart@hotmail.com>
2025-03-20 13:43:55 +0100

    Tercera confirmacion

---
+++ src/controlversiones2/Segunda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlversiones2;
+
+/**
+ *
+ * @author Maria8
+ */
+public class Segunda {
+    public void exx(){
+        System.out.println("rama final");
+    }
+}
